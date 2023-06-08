08bc4a639752b11c6ccdf97490bc2fec4f1b47dd
VarshiniSathishkannan <varshinisathish2112@gmail.com>
2023-06-08 18:49:21 +0530

    basics

---
+++ basics.sql
@@ -0,0 +1,15 @@
+declare a int 0; /* Local scope */
+
+set @a = 0 /* Global scope */
+
+create view names_with_m as select * from student where name like '%m%';
+
+select * from names_with_m;
++----+--------+
+| id | name   |
++----+--------+
+|  3 | manoj  |
+|  4 | mayils |
+|  5 | Magesh |
++----+--------+
+
